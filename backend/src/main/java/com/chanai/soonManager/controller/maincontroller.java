c2ee51c590d4a3c84fc9ffb8b3c62b11304acd98
Chanyoung3 <33cksdud@gmail.com>
2026-04-03 23:37:43 +0900

    Backend folder create

---
+++ backend/src/main/java/com/chanai/soonManager/controller/maincontroller.java
@@ -0,0 +1,9 @@
+package com.chanai.soonManager.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class maincontroller {
+     
+}
