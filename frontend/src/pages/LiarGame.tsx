d52da49a3dca327e976b864ca90608cf32b00a19
Chanyoung3 <33cksdud@gmail.com>
2026-04-25 23:51:33 +0900

    LiarGame 7

--- frontend/src/pages/LiarGame.tsx
+++ frontend/src/pages/LiarGame.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import Header from "../components/Header";
 
 const LiarGame = () => {
-    return(
-        <Header />
-        
+    return (
+        <>
+            <Header />
+            <textarea placeholder="설명을 입력하세요"></textarea>
+            <h1>이름</h1>
+
+            <div className="ex_list">
+
+            </div>
+        </>
+
     );
 };
 
